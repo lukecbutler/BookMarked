6d1f0896348a02e809a4a41a274491281ee474e7
Jake Rouse <jakerouse0503@gmail.com>
2025-10-23 10:53:15 -0400

    Sprint1Deliverable+Sprint1db files
    - Created the .py for the sprint1deliverable app
    
    - created the db instance for the sprint 1 deliverable

---
+++ Sprint1Deliverable.py
@@ -0,0 +1,67 @@
+from flask import Flask, request, jsonify, render_template_string
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Sprint1db.db'
+
+database = SQLAlchemy(app)
+
+class LibraryBranch(database.Model):
+
+    __tablename__ = 'LibraryBranch'
+
+    BranchID = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    BranchName = database.Column(database.String(50))
+
+class ItemType(database.Model):
+
+    __tablename__ = 'ItemType'
+
+    TypeID = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    TypeName = database.Column(database.String(30))
+    RentalLength = database.Column(database.Integer)
+    PerDayFine = database.Column(database.Numeric(10, 2))
+
+class LibraryItem(database.Model):
+
+    __tablename__ = 'LibraryItem'
+
+    ItemID = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    ItemType = database.Column(database.Integer, database.ForeignKey('ItemType.TypeID'))
+    AquisitionDate = database.Column(database.Date)
+    Cost = database.Column(database.Numeric(10, 2))
+    ItemTitle = database.Column(database.String(50))
+    Availability = database.Column(database.Boolean)
+    ShelfCode = database.Column(database.String(5))
+
+class Patron(database.Model):
+    
+    __tablename__ = 'Patron'
+
+    PatronID = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    PatronFN = database.Column(database.String(50))
+    PatronLN = database.Column(database.String(50))
+    AccountExpDate = database.Column(database.Date)
+    FeesOwed = database.Column(database.Numeric(10, 2))
+    ItemsCheckedOut = database.Column(database.Integer)
+
+class Checkout(database.Model):
+
+    __tablename__ = 'Checkout'
+
+    TransactionID = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    PatronID = database.Column(database.Integer, database.ForeignKey('Patron.PatronID'))
+    ItemID = database.Column(database.Integer, database.ForeignKey('LibraryItem.ItemID'))
+    CheckoutDate = database.Column(database.Date)
+
+class Return(database.Model):
+
+    __tablename__ = 'Return'
+
+    TransactionID = database.Column(database.Integer, database.ForeignKey('Checkout.TransactionID'), primary_key=True)
+    DateReturned = database.Column(database.Date)
+    BranchReturnedTo = database.Column(database.Integer, database.ForeignKey('LibraryBranch.BranchID'))
+
+with app.app_context():
+    database.create_all()
